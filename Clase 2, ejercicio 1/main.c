0e6a116caa4dcc75e20f9d7c2e25f8f9b3904500
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-03-21 12:03:05 -0300

    Laboratorio 1
    Primera clase, minimo y maximo

---
+++ Clase 2, ejercicio 1/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+    int i;
+    int suma = 0;
+    int max; // constante definida en limits.h
+    int min; // constante definida en limits.h
+    float promedio;
+
+    for(i=0; i<10;i++)
+    {
+    int aux;
+    scanf("%d",&aux);
+    suma=suma+aux;
+
+    if(i == 0)
+    {
+        max = aux;
+        min = aux;
+    }
+    if(aux>max)
+    {
+        max = aux;
+    }
+    if(aux<min)
+    {
+        min = aux;
+    }
+    }
+    promedio = (float)suma/10;
+
+    printf("la suma es:%d\n" , suma);
+    printf("El maximo es:%d\n" , max);
+    printf("El minimo es:%d\n" , min);
+    printf("El promedio es:%.2f" , promedio);
+
+}
