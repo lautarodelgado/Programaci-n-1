985ed5214fb234947b2ea139467ca2dde9938b08
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-03-26 12:28:11 -0300

    Funcion sumar

--- Funcion sumar/main.c
+++ Funcion sumar/main.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int sumar(int , int);
+#include "baus.h" //como no esta en las librerias incluidas va con comillas
 
 int main()
 {
     int x=5;
     int y=7;
 
-    printf("La suma es %d\n", sumar(x , y));
+    printf("La suma es %d\n", sumar(x , y));  //o podemos no declarar como "x" e "y" y reemplazar por los valores en el paréntesis
 
     return 0;
 }
 
-int sumar(int a , int b)
-{
-    int resultado;
-    resultado = a + b;
 
-    return resultado;
-}
