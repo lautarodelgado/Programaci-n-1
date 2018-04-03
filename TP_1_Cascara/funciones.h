9c8a51a86768ed7ddb573903962fd59922681ae3
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-04-03 20:51:12 -0300

    Add files via upload

---
+++ TP_1_Cascara/funciones.h
@@ -0,0 +1,11 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+int suma(int , int);
+int resta(int , int);
+float division(int , int);
+int multiplicacion(int , int);
+int aa(int);
+
+#endif // FUNCIONES_H_INCLUDED
+
