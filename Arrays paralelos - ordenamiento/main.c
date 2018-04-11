4b7c5aa587a605dbded5f0ad73448c1ebd2e0d5f
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-04-11 11:55:12 -0300

    Add files via upload

--- Arrays paralelos - ordenamiento/main.c
+++ Arrays paralelos - ordenamiento/main.c
@@ -9,6 +9,8 @@ int main()
     char nombre[ELEMENTOS][50] = {"Pedro","Carlos","Juan","Ernesto","Mario"};
     char apellido[ELEMENTOS][50]= {"Gomez","Fernandez","Paz","Perez","Lopez"};
     char auxiliar[50];
+    int edad[ELEMENTOS] = {23 , 44 , 52 , 54 ,78};
+    int auxiliarInt;
     for(i=0;i< ELEMENTOS-1;i++)
     {
         for(j=i+1;j<ELEMENTOS;j++)
@@ -22,13 +24,17 @@ int main()
             strcpy(auxiliar,apellido[i]);
             strcpy(apellido[i],apellido[j]);
             strcpy(apellido[j], auxiliar);
+
+            auxiliarInt = edad[i];
+            edad[i] = edad[j];
+            edad[j] = edad[i];
             }
         }
     }
     printf("\n\nLista ordenada por nombres");
     for(i=0;i<ELEMENTOS;i++)
     {
-        printf("\nNombre: %s - Apellido: %s",nombre[i],apellido[i]);
+        printf("\nNombre: %s - Apellido: %s - Edad: %d",nombre[i],apellido[i] , edad[i]);
     }
 
     return 0;
