ae3ff404a50ba54654595fbc641a482b0b04b7af
lautarodelgado <36193724+lautarodelgado@users.noreply.github.com>
2018-04-16 11:58:47 -0300

    Add files via upload

--- Estructura/main.c
+++ Estructura/main.c
@@ -11,24 +11,40 @@ typedef struct
     char sexo;
 }eEmpleado;
 
+void mostrarEmpleado(eEmpleado);
 
 int main()
 {
-    eEmpleado unEmpleado; //se puede harcodear pero en el mismo orden en el que esta la estructura
+    eEmpleado unEmpleado = {"Juan" , 1234 , 50000.50 , 'f'}; //se puede harcodear pero en el mismo orden en el que esta la estructura
 
+    eEmpleado otroEmpleado = unEmpleado; //puedo copiar estructuras con un =
+
+    /*
     unEmpleado.legajo = 1234;
     strcpy(unEmpleado.nombre , "Juan");
     unEmpleado.sueldo = 50000.50;
     unEmpleado.sexo = 'f';
 
-    /*
     printf("ingrese legajo");
     scanf("%d" , &unEmpleado.legajo);
     */
 
+    /*
     printf("Nombre: %s\n" , unEmpleado.nombre);
     printf("Legajo: %d\n" , unEmpleado.legajo);
     printf("Sueldo: %.2f\n" , unEmpleado.sueldo);
     printf("Sexo: %c\n" , unEmpleado.sexo);
+    */
+
+    //mostrarEmpleado(unEmpleado);
+    mostrarEmpleado(otroEmpleado);
     return 0;
+}
+
+void mostrarEmpleado(eEmpleado emp)
+{
+    printf("Nombre: %s\n" , emp.nombre);
+    printf("Legajo: %d\n" , emp.legajo);
+    printf("Sueldo: %.2f\n" , emp.sueldo);
+    printf("Sexo: %c\n" , emp.sexo);
 }
